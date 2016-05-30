c3ec3d204b28e2ca58aa9f3a940391fb0e9d0cb0
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-30 14:43:19 +0530

    thruk_install

--- thruk_install/recipes/default.rb
+++ thruk_install/recipes/default.rb
@@ -1,23 +1,37 @@
 #add not_if dir.exist
+directory '/opt/thruk' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+package 'gdebi'
 
 #1
-
 remote_file "/opt/thruk/libthruk_2.08-1_ubuntu14.04_amd64.deb" do
   source "http://download.thruk.org/pkg/v2.08/ubuntu14.04/amd64/libthruk_2.08-1_ubuntu14.04_amd64.deb"
   mode 0644
+  notifies :run, "execute[libthruk]", :immediately
 end
 
-dpkg_package "/opt/thruk/libthruk_2.08-1_ubuntu14.04_amd64.deb" do
-  ignore_failure true
-  action :install
-  notifies :run, "execute[install-libthruk-deps]", :immediately
-end
 
-execute "install-libthruk-deps" do
-  command "apt-get -yf install"
+execute "libthruk" do
+  command "sudo gdebi  --option=APT::Get::force-yes='true' --option=APT::Get::Assume-Yes='true' -n libthruk_2.08-1_ubuntu14.04_amd64.deb"
   action :nothing
 end
 
+#dpkg_package "/opt/thruk/libthruk_2.08-1_ubuntu14.04_amd64.deb" do
+#  ignore_failure true
+#  action :install
+#  notifies :run, "execute[install-libthruk-deps]", :immediately
+#end
+#
+#execute "install-libthruk-deps" do
+#  command "apt-get -yf install"
+#  action :nothing
+#end
+#
 
 #2
 remote_file "/opt/thruk/thruk-base_2.08_ubuntu14.04_amd64.deb" do
@@ -25,18 +39,23 @@ remote_file "/opt/thruk/thruk-base_2.08_ubuntu14.04_amd64.deb" do
   mode 0644
 end
 
-
-dpkg_package "/opt/thruk/thruk-base_2.08_ubuntu14.04_amd64.deb" do
-  ignore_failure true
-  action :install
-  notifies :run, "execute[install-thrukbase-deps]", :immediately
-end
-
-execute "install-thrukbase-deps" do
-  command "apt-get -yf install"
+execute "thruk-base" do
+  command "sudo gdebi  --option=APT::Get::force-yes='true' --option=APT::Get::Assume-Yes='true' -n thruk-base_2.08_ubuntu14.04_amd64.deb"
   action :nothing
 end
 
+#
+#dpkg_package "/opt/thruk/thruk-base_2.08_ubuntu14.04_amd64.deb" do
+#  ignore_failure true
+#  action :install
+#  notifies :run, "execute[install-thrukbase-deps]", :immediately
+#end
+#
+#execute "install-thrukbase-deps" do
+#  command "apt-get -yf install"
+#  action :nothing
+#end
+#
 
 
 
@@ -47,17 +66,22 @@ remote_file "/opt/thruk/thruk-plugin-reporting_2.08_ubuntu14.04_amd64.deb" do
 end
 
 
-dpkg_package "/opt/thruk/thruk-plugin-reporting_2.08_ubuntu14.04_amd64.deb" do
-  ignore_failure true
-  action :install
-  notifies :run, "execute[install-plugin-deps]", :immediately
-end
-
-execute "install-plugin-deps" do
-  command "apt-get -yf install"
+execute "thruk-plugin" do
+  command "sudo gdebi  --option=APT::Get::force-yes='true' --option=APT::Get::Assume-Yes='true' -n thruk-plugin-reporting_2.08_ubuntu14.04_amd64.deb"
   action :nothing
 end
 
+#dpkg_package "/opt/thruk/thruk-plugin-reporting_2.08_ubuntu14.04_amd64.deb" do
+#  ignore_failure true
+#  action :install
+#  notifies :run, "execute[install-plugin-deps]", :immediately
+#end
+#
+#execute "install-plugin-deps" do
+#  command "apt-get -yf install"
+#  action :nothing
+#end
+#
 
 #4
 remote_file "/opt/thruk/thruk_2.08_ubuntu14.04_amd64.deb" do
@@ -66,14 +90,20 @@ remote_file "/opt/thruk/thruk_2.08_ubuntu14.04_amd64.deb" do
 end
 
 
-dpkg_package "/opt/thruk/thruk_2.08_ubuntu14.04_amd64.deb" do
-  ignore_failure true
-  action :install
-  notifies :run, "execute[install-thruk-deps]", :immediately
-end
-
-execute "install-thruk-deps" do
-  command "apt-get -yf install"
+execute "thruk" do
+  command "sudo gdebi  --option=APT::Get::force-yes='true' --option=APT::Get::Assume-Yes='true' -n thruk_2.08_ubuntu14.04_amd64.deb"
   action :nothing
 end
 
+#
+#dpkg_package "/opt/thruk/thruk_2.08_ubuntu14.04_amd64.deb" do
+#  ignore_failure true
+#  action :install
+#  notifies :run, "execute[install-thruk-deps]", :immediately
+#end
+#
+#execute "install-thruk-deps" do
+#  command "apt-get -yf install"
+#  action :nothing
+#end
+#
